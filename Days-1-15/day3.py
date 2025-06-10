e005d6869ab7928c74052f548f5e880c9ee71165
Xavier Lozada <xavicoop5@gmail.com>
2025-06-10 14:14:38 -0400

    Day three complete with One Piece treasure hunt

--- Days-1-15/day3.py
+++ Days-1-15/day3.py
@@ -1,32 +1,146 @@
 '''
-This is day 3, I think we're making a small game
+ALL COMMENTED OUT CODE IS WORKING OR BEING TEMPORARILY BLOCKED OUT FOR TESTING PURPOSES
+This is day 3, I think we'remaking a small game
 '''
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm?"))
-bill = 0
-if height >= 120:
-    print("You can ride the rollercoaster!")
-    age = int(input("How old are you?"))
-    if age <= 17:
-        bill = 4
-        print("Children pay $4")
-    elif age <= 43:
-        bill = 13
-        print("Adults pay $13")
-    else :
-        bill = 19
-        print("Seniors $19 please and thanks ")
-    photo = input("Do you want a photo taken?")
-    if photo == "y":
-        print("This adds $3 dollars to the bill")
-        if True:
-            print(bill + 3)
-        else :
-            print(bill)
-else :
-    print("You're too short shorty!")
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm?"))
+# BILL = 0
+# if height >= 120:
+    # print("You can ride the rollercoaster!")
+    # age = int(input("How old are you?"))
+    # if age <= 17:
+        # BILL = 4
+        # print("Children pay $4")
+    # elif age <= 43:
+        # BILL = 13
+        # print("Adults pay $13")
+    # else :
+        # BILL = 19
+        # print("Seniors $19 please and thanks ")
+    # photo = input("Do you want a photo taken?(y/n)")
+    # if photo == "y":
+        # print("This adds $3 dollars to the bill\n ")
+        # print("Your final bill is \n" , BILL + 3, sep = '💲')
+    # else :
+        # print("You Final bill is " , BILL , sep = '💲')
+# else :
+    # print("You're too short shorty!")
 # number = int(input("Pick a number and I will tell you if it is odd or even "))
 # if  number % 2 == 0:
 #     print("That number is even")
 # else :
 #     print("That number is odd")
+#
+#
+# Python Pizza Project
+# print("Welcome to Pookie's Pizzaria!\n")
+# print("We have many fantastic options for our pizzas.\n")
+# print("Please choose a size as well as toppings,\n")
+# print("but be aware of price increases for each topping!\n")
+# size = input("What size pizza do you want, S, M, or L? ").upper()
+# extra_cheese = input("Would you like extra cheese on it, y or n? ").upper()
+# pepperoni = input("Would you like pepperoni on it, y or n? ").upper()
+# sausage = input("Would you like sausage on it, y or n? ").upper()
+# pineapple = input("Would you like pineapple on it, y or n? ").upper()
+# half_and_half = input("Would you like a half and half pizza, y or n? ").upper()
+# TOTAL_PRICE = 0
+# if size == "S" :
+#     TOTAL_PRICE+=10
+# elif size == "M" :
+#     TOTAL_PRICE+=14
+# elif size == "L" :
+#     TOTAL_PRICE=18
+# else :
+#     print("Invalid size. Please restart and select S, M, or L.")
+#     exit()
+# if TOTAL_PRICE > 0 :
+#     if extra_cheese == "Y" :
+#         TOTAL_PRICE+=3
+#     if pepperoni == "Y" :
+#         TOTAL_PRICE+=3
+#     if sausage == "Y" :
+#         TOTAL_PRICE+=3
+#     if pineapple == "Y" :
+#         TOTAL_PRICE+=3
+#     if half_and_half == "Y" :
+#         TOTAL_PRICE+=1
+# print("Your total is ", str(TOTAL_PRICE), sep= "💲")
+# Final day 3 project, Raftel Hunt
+print('''
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⠀⠀⠀⢠⣾⣧⣤⡖⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⠋⠀⠉⠀⢄⣸⣿⣿⣿⣿⣿⣥⡤⢶⣿⣦⣀⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡆⠀⠀⠀⣙⣛⣿⣿⣿⣿⡏⠀⠀⣀⣿⣿⣿⡟
+⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⠷⣦⣤⣤⣬⣽⣿⣿⣿⣿⣿⣿⣿⣟⠛⠿⠋⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠋⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⡆⠀⠀
+⠀⠀⠀⠀⣠⣶⣶⣶⣿⣦⡀⠘⣿⣿⣿⣿⣿⣿⣿⣿⠿⠋⠈⢹⡏⠁⠀⠀
+⠀⠀⠀⢀⣿⡏⠉⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡆⠀⢀⣿⡇⠀⠀⠀
+⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣟⡘⣿⣿⣃⠀⠀⠀
+⣴⣷⣀⣸⣿⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⠹⣿⣯⣤⣾⠏⠉⠉⠉⠙⠢⠀
+⠈⠙⢿⣿⡟⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣄⠛⠉⢩⣷⣴⡆⠀⠀⠀⠀⠀
+⠀⠀⠀⠋⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣀⡠⠋⠈⢿⣇⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⠿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+''')
+DEATH = '''
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡖⠉⠉⢦⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⡰⠊⠉⢢⠀⠀
+ ⡔⠉⠁⠀⠀⢎⠀⠀⠀⣠⠖⠋⠀⠀⠀⠀⠀⠈⠙⠢⣄⠀⠀⠀⡱⠀⠀⠈⠙⢂
+ ⠣⣀⣀⢄⠀⠀⠑⢤⠞⢡⠎⡴⢠⠀⠀⠀⠀⠀⢆⠐⡌⠳⡤⠊⠀⠀⡠⣀⣀⠜
+ ⠀⠀⠀⠀⠑⢄⣀⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⡠⠊⠀⠀⠀⠀
+ ⠀⠀⢠⠤⠤⠤⠽⠿⠾⠿⠷⠿⠾⠷⠿⠾⠷⠿⠾⠷⠿⠾⠷⠯⠤⠤⠤⢄⠀⠀
+ ⠀⠀⠈⠉⠉⠉⠹⡉⠉⠉⣉⣭⣍⡉⠉⠉⠉⣩⣭⣉⠉⠉⠉⡏⠉⠉⠉⠁⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⢳⠀⠰⣿⣿⣿⣷⠀⠀⣾⣿⣿⣿⡇⠀⡼⠀⠀⠀⠀⠀⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠱⣄⠙⠿⠟⠋⢀⣀⠘⠛⠿⠛⣠⠞⠀⠀⠀⠀⠀⠀⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⡨⡗⠢⢤⢀⣈⣉⡀⡤⠖⢾⢅⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⡠⠊⢰⠓⠢⡾⣀⣀⣇⣀⣧⠄⠺⡆⠑⢤⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢀⠊⠉⠑⠋⠀⢀⣸⠁⠲⠧⢀⣀⣇⣀⠸⠔⠊⣇⠀⠀⠑⠊⠉⠲⡀⠀⠀
+ ⠀⠀⠈⢤⣀⠀⠀⠰⡍⠈⢃⠀⠀⠀⠀⠀⠀⠀⠀⡸⠁⢱⠂⠀⠀⣀⡰⠁⠀⠀
+ ⠀⠀⠀⠀⠘⢄⣀⡠⠃⠀⠀⠑⠤⠄⠀⠀⠠⠤⠊⠀⠀⠘⢄⣀⡠⠃⠀⠀⠀⠀
+                YOU DIED
+ '''
+ONE_PIECE = '''⠀⠀⠀⠰⠁⠀⠀⠀⠀⠀⢀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ ⠀⠀⠀⡰⠁⠀⠀⠀⠀⠀⠀⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ ⠀⢀⠎⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⠟⠁⠀⣠⡔⠀⠀⠀⠀⠀⠀⠀⠀
+ ⠐⠁⠀⠀⠀⠀⢀⡀⢀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣶⣶⡖⠁⠀⠀⠀
+ ⠀⠀⠀⠀⢠⣾⣿⣯⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣤⣀⣀⠀
+ ⣶⣄⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⠏⢻⣿⣿⣿⣿⣿⣿⣿⣿⣏⠀⠀
+ ⣯⢿⣆⠀⣠⣾⣿⣿⣿⣿⣿⣿⡿⠁⠀⠸⡟⣿⡇⠘⣿⣿⣿⣿⣿⣷⣄
+ ⣿⣿⣿⡯⢠⣿⣿⡿⣿⣿⢿⠏⠀⠂⠄⡀⠁⠹⡇⠀⢻⣿⣿⣿⣿⣿⠀
+ ⣿⣿⣿⣿⠾⣿⡇⣫⡾⡏⠎⢀⡤⠤⣄⠀⠀⠀⢁⠠⠄⢽⢻⣿⣿⠿⠀
+ ⢏⣿⣿⣿⣆⢻⣷⡣⣧⠅⢀⡈⠀⠀⠈⠀⠀⠀⡇⠤⠤⡀⢸⣿⣿⠀⠀
+ ⣿⣿⣿⣿⢻⣯⡸⠋⠺⡆⠘⡟⠒⠂⠤⠀⢀⣐⡁⠒⠐⢢⡿⠁⠈⠃⠀
+ ⣿⣿⣿⣿⣿⣿⡆⠀⠀⣷⣄⠹⢏⠁⠐⠒⠠⢤⣄⣝⣠⣋⣀⡀⠀⠀⠀
+ ⣿⣿⣿⣿⣿⣿⣷⠀⠀⣸⣿⣷⡤⡁⠂⠤⠤⠔⡋⠔⠁⠀⠀⠉⠀⠀⠀
+ ⣿⣿⣿⣿⣿⣻⣿⣦⡀⠸⣿⣿⡟⠉⠁⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ ⣿⣿⣿⣿⢿⢿⣿⣯⠳⣄⣽⣿⣷⡀⠀⠀⠀
+        YOU FOUND THE ONE PIECE!
+ '''
+
+print("Welcome to Raftel, where you are in search of the One Piece ")
+print("Your journey will be treacherous so think carefully about each decision. ")
+print("Good luck on your journey Pirate! ")
+print("CRAAAASSSHHH!!! ")
+print("''What happened''??? ")
+
+# PROMPTS
+PATH_CHOICE = "You arrive on the island and notice a passage, left or right?\n"
+RIVER_CHOICE = "You can either swim or wait.\n"
+DOOR_CHOICE = "Pick a door: red, blue, or yellow. Reveal your fate!\n"
+path_choice = input(PATH_CHOICE)
+if path_choice == "left":
+    river_choice = input(RIVER_CHOICE)
+    if river_choice.lower() == "wait":
+        door_choice = input(DOOR_CHOICE)
+        if door_choice.lower() == "red":
+            print(ONE_PIECE)
+        elif door_choice.lower() == "blue" or door_choice == "yellow":
+            print("You picked the door of Death" , DEATH)
+        else:
+            print("You need to pick a door!")
+    elif river_choice.lower() == "swim":
+        print("You have been eaten by sharks!" , DEATH)
+    else :
+        print("You have to pick between swim or wait!")
+elif path_choice == "right":
+    print("You fell into a deep dark hole!" , DEATH)
+else:
+    print("Invalid choice, please choose a path!")
